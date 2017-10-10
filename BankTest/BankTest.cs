bd8be9512cbe09f580b6e6aabd8edb94e6a783d0
shanthikannan.d <shanthikannan.d@gmail.com>
2017-10-10 16:47:11 -0400

    BankTest

---
+++ BankTest/BankTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTest
+{
+    [TestClass]
+    public class BankTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrage 
+            double balance = 1000.00;
+            double debitAmount = 100.00;
+            double expectedBalance = 900.00;
+
+            Bank.Class1.BankAccount bank = new Bank.Class1.BankAccount("Shanthi", balance);
+
+            //Act
+            bank.Debit(debitAmount);
+
+
+            //Assert
+            double actualBalance = bank.Balance;
+            Assert.AreEqual(expectedBalance, actualBalance);
+       
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestOutOfRangeException()
+        {
+            //Arrage 
+            double balance = 100.00;
+            double debitAmount = 200.00;
+            Bank.Class1.BankAccount bank = new Bank.Class1.BankAccount("Shanthi", balance);
+
+            //Act
+            bank.Debit(debitAmount);
+           
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestOutOfRangeException2()
+        {
+            //Arrage 
+            double balance = 100.00;
+            double debitAmount = -200.00;
+            Bank.Class1.BankAccount bank = new Bank.Class1.BankAccount("Shanthi", balance);
+
+            //Act
+            bank.Debit(debitAmount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestFrozenAccount()
+        {
+            //Arrage 
+            double balance = 100.00;
+            double debitAmount = 50.00;
+            Bank.Class1.BankAccount bank = new Bank.Class1.BankAccount("Shanthi", balance);
+            bank.FreezeAccount();
+
+            //Act
+            bank.Debit(debitAmount);
+        }
+
+    }
+}
---
+++ BankTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PCUser")]
+[assembly: AssemblyProduct("BankTest")]
+[assembly: AssemblyCopyright("Copyright © PCUser 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8487ec5-b98b-43b6-ba08-1bed0725c454")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
